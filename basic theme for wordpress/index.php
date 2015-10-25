8fa60537b7df3a68c625c48efb975c8302ff7ccc
orYoffe <ragiop07@gamil.com>
2015-10-25 13:03:34 +0100

    small fix to the index.php (template)

--- basic theme for wordpress/index.php
+++ basic theme for wordpress/index.php
@@ -7,8 +7,8 @@
 		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<?php the_content() ?>	
 		<?php endwhile;
-	else:
-		echo '<p>No content found</p>'
+	else :
+		echo '<p>No content found</p>';
 	endif;
 
 	get_footer();
